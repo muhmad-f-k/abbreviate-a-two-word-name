a85175cd5521c65db3aed049f0af66ac741a9ca3
Muhmad K. Fauzi <muhmad@live.no>
2022-10-24 19:06:19 +0200

    small fix, less code

--- abbrev_name.py
+++ abbrev_name.py
@@ -1,6 +1,5 @@
 def abbrev_name(name):
     split_name = name.split(" ")
-    get_first_char =split_name[0][0]+"." + split_name[1][0]
-    final_list = get_first_char.upper()
-    return final_list
+    get_first_char =split_name[0][0]+"." + split_name[1][0].upper()
+    return get_first_char
 print(abbrev_name(name="Sam Harris")) # Forventet svar S.H
